babdb34cbae3fda8b535e7580e4ca6719c357949
MisaelTox <misaeltoxcatl@gmail.com>
2025-10-28 16:04:16 +0100

    Update network and add keypair configuration

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-north-1a"
-  map_public_ip_on_launch = true   # 👈 fuerza IP pública
+  map_public_ip_on_launch = true # 👈 fuerza IP pública
 
   tags = {
     Name = "${var.project_name}-subnet"

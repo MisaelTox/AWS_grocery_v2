15fdfa5fdd06ce9aa63ee07a9cdb3812bb2beee2
MisaelTox <misaeltoxcatl@gmail.com>
2025-10-28 15:44:49 +0100

    add EC2 user data script to deply GroceryMate Docker container automatically

--- infrastructure/user_data.sh
+++ infrastructure/user_data.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # === User Data Script for GroceryMate EC2 (by MisaelTox) ===
 
-# Actualizar el sistema
-apt update -y
-apt upgrade -y
+# Actualizar sistema
+yum update -y
 
 # Instalar dependencias
-apt install -y git docker.io
+yum install -y git docker
 systemctl enable docker
 systemctl start docker
 
-# Clonar repositorio (versión limpia)
-cd /home/ubuntu
+# Clonar repositorio
+cd /home/ec2-user
 git clone https://github.com/MisaelTox/AWS_grocery_v2.git
 cd AWS_grocery_v2
 
@@ -19,6 +18,5 @@ cd AWS_grocery_v2
 docker build -t grocerymate .
 docker run -d -p 80:80 grocerymate
 
-# Guardar un registro de la instalación
-echo "GroceryMate v2 (by MisaelTox) deployed successfully" > /home/ubuntu/deploy.log
-repo
+# Log
+echo "GroceryMate v2 (by MisaelTox) deployed successfully" > /home/ec2-user/deploy.log
